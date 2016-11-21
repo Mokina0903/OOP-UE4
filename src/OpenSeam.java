3895cc36d100c880ecf081ec5f2c5a37b17706d4
1468ice <dataswanice@yahoo.de>
2016-11-21 10:17:26 +0100

    OpenSeam constructor

--- src/OpenSeam.java
+++ src/OpenSeam.java
@@ -4,18 +4,13 @@ public class OpenSeam extends Fruit implements DurableSquirrelFood {
 	private int seams;
 
 	public OpenSeam(Fruit fruit) {
-		if (fruit instanceof Cone) {
-			this.carb = fruit.carb * 2;
-			this.fat = fruit.fat * 2;
-			this.protein = fruit.protein * 2;
+		int factor = (fruit instanceof Cone ? 2 : 1);
+			this.carb = fruit.carb * factor;
+			this.fat = fruit.fat * factor;
+			this.protein = fruit.protein * factor;
 			this.eatWithinDays = (fruit).eatWithinDays();
 
-		} else if (fruit instanceof Nut) {
-			this.carb = fruit.carb;
-			this.fat = fruit.fat;
-			this.protein = fruit.protein;
-			this.eatWithinDays = (int) ((fruit).eatWithinDays() * 0.6);
-		}
+			this.eatWithinDays = (int) ((fruit).eatWithinDays() * (factor/2));
 	}
 
 	public String toString() { return "Open Seam: " + super.toString(); }
