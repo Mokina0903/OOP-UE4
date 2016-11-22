f1b5669f3b4c385aeed160b64ffb9000aec1b6ea
Mokina0903 <monika.wiszmann@gmail.com>
2016-11-22 20:56:33 +0100

    Begründung fehlende Untertypen

--- src/Test.java
+++ src/Test.java
@@ -12,6 +12,19 @@ public class Test {
     Monika:
     Samuel:
 
+    Keine Untertypbeziehungen zwischen:
+        Cone, Nut und OpenSeam:
+        Die drei Klassen stehen auf einer Ebene. Nut bzw. Cone koennen durch die Methode crack() in die Klasse
+        OpenSeam "ueberfuehrt" werden. Es ist keine Untertypbeziehung vorhanden, da diese keine Methoden voneinander
+        erben, welche nicht bereits von Fruit vererbt werden.
+
+        FreshFruit und DryFruit:
+        Auch hier sind die Klassen auf einer Ebene anzusehen. Es besteht keine Untertypbeziehung da ein FreshFruit
+        keine DryFruit ist oder umgekehrt. Es soll nur eine Freshfruit in eine Dryfruit ueberfuehrt werden.
+
+        Cone, Nut, OpenSeam, FreshFruit und DryFruit sind die Blaetter, wuerde man die Beziehungen in einem Baumdiagramm
+        festhalten.
+
     */
 
     public static void main(String[] args) {
@@ -25,6 +38,7 @@ public class Test {
         System.out.println(dryFruit);
         Cone cone = new Cone();
         System.out.println(cone);
+        System.out.println(cone.seams());
         System.out.println(cone.crack());
 
     }
